1531c5c907e86e523c2e6f17b94d75d9f2e5aaab
heya <heyabird@gmail.com>
2020-12-10 23:32:53 -0800

    add filter box

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,13 @@ import Project from '../components/Project';
 
 export const About = () => (
   <>
-    <h2>Code</h2>
-    <p>Click on a project to see more.</p>
+    <div class="code-header">
+      <div>
+        <h2>Code</h2>
+        <p>Click on a project to see more.</p>
+      </div>
+      <div class="filter">Filter</div>
+    </div>
     <Project/>
   </>
 )
