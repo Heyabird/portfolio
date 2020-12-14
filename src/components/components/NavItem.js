07b359318d0b72a2e7937c9181473648d7d20970
heya <heyabird@gmail.com>
2020-12-13 21:21:15 -0800

    removed the step using NavItem.js and instead put all the nav login in Sidebar.js

--- src/components/components/NavItem.js
+++ src/components/components/NavItem.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { StyledNavItem } from './StyledNavItem';
 import styled from "styled-components";
-import heya from '../../icons/heya.svg';
+import  heya from './heya.svg'
 
 
 export default class NavItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      heya: 'heya',
+      icon: heya
     }
   }
 
@@ -18,13 +18,17 @@ export default class NavItem extends React.Component {
     onItemClick(path);
   }
 
+
   render() {
+    console.log(this.props.icon);
+    console.log(typeof this.props.icon);
     const { active } = this.props;
     const NavIcon = styled.div`x`;
     return (
       <StyledNavItem active={active}>
         <Link to={this.props.path} onClick={this.handleClick}>
           <NavIcon></NavIcon>
+          {/* <img src={this.props.iconUrl} alt="icon"/> */}
           <img src={heya} alt="icon"/>
           <p class="nav-text">{this.props.name}</p>
         </Link>
--- src/components/components/Sidebar.js
+++ src/components/components/Sidebar.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { StyledSideNav } from './StyledSideNav';
+import { StyledNavItem } from './StyledNavItem';
 import NavItem from './NavItem';
+import heya from './heya.svg'
+import code from './code.svg'
+import design from './design.svg'
+import music from './music.svg'
+
+
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class SideNav extends React.Component {
   constructor(props) {
@@ -12,46 +22,88 @@ class SideNav extends React.Component {
           path: '/', /* path is used as id to check which NavItem is active basically */
           name: 'Heya',
           css: 'fa fa-fw fa-circle',
-          key: 1 /* Key is required, else console throws error. Does this please you Mr. Browser?! */
+          key: 1 /* Key is required, else console throws error. Does this please you Mr. Browser?! */,
+          iconUrl: `{heya}`
         },
         {
           path: '/about',
           name: 'Code',
           css: 'fa fa-fw fa-circle',
-          key: 2
+          key: 2,
+          iconUrl: ''
         },
         {
           path: '/design',
           name: 'Design',
           css: 'fas fa-circle',
-          key: 3
+          key: 3,
+          iconUrl: ''
         },
         {
           path: '/music',
           name: 'Music',
           css: 'fas fa-circle',
-          key: 4
+          key: 4,
+          iconUrl: ''
         },
       ]
     }  
   }
   onItemClick = (path) => {
+    console.log("path: ", path)
     this.setState({ activePath: path }); /* Sets activePath which causes rerender which causes CSS to change */
   }
+  handleClick = () => {
+    const { path, onItemClick } = this.props;
+    onItemClick(path);
+  }
+
   render() {
     const { items, activePath } = this.state;
+    const { active } = true;
+    const NavIcon = styled.div`x`;
     return (
       <StyledSideNav>
-        {
-          /* items = just array AND map() loops thru that array AND item is param of that loop */
-          items.map((item) => {
-            /* Return however many NavItems in array to be rendered */
-            return (
-              <NavItem path={item.path} name={item.name} css={item.css} icon={item.icon} onItemClick={this.onItemClick} /* Simply passed an entire function to onClick prop */ active={item.path === activePath} key={item.key}/>
-            )
-          })
-        }
+        <StyledNavItem active={active}>
+          <Link to={items[0].path} onClick={this.onItemClick}>
+            <NavIcon></NavIcon>
+            <img src={heya} alt="icon"/>
+            <p class="nav-text">heya</p>
+          </Link>
+        </StyledNavItem>
+        <StyledNavItem active={active}>
+            <Link to={items[1].path} onClick={this.onItemClick}>
+              <NavIcon></NavIcon>
+              <img src={code} alt="icon"/>
+              <p class="nav-text">code</p>
+            </Link>
+        </StyledNavItem>
+        <StyledNavItem active={active}>
+            <Link to={items[2].path} onClick={this.onItemClick}>
+              <NavIcon></NavIcon>
+              <img src={design} alt="icon"/>
+              <p class="nav-text">design</p>
+            </Link>
+        </StyledNavItem>
+        <StyledNavItem active={active}>
+            <Link to={items[3].path} onClick={this.onItemClick}>
+              <NavIcon></NavIcon>
+              <img src={music} alt="icon"/>
+              <p class="nav-text">music</p>
+            </Link>
+        </StyledNavItem>
       </StyledSideNav>
+      // <StyledSideNav>
+      //   {
+      //     /* items = just array AND map() loops thru that array AND item is param of that loop */
+      //     items.map((item) => {
+      //       /* Return however many NavItems in array to be rendered */
+      //       return (
+      //         <NavItem path={item.path} name={item.name} css={item.css} icon={item.icon} onItemClick={this.onItemClick} /* Simply passed an entire function to onClick prop */ active={item.path === activePath} key={item.key} icon={item.iconUrl}/>
+      //       )
+      //     })
+      //   }
+      // </StyledSideNav>
     );
   }
 }
