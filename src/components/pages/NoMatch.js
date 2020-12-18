c0f01080016f74c35f00dc8749cf117c69dd9dd9
heya <heyabird@gmail.com>
2020-12-18 10:41:07 -0800

    update bandcamp link

--- src/components/pages/NoMatch.js
+++ src/components/pages/NoMatch.js
@@ -14,7 +14,7 @@ export const NoMatch = () => (
           <Button href="https://soundcloud.com/heyaworld" target="_blank"><i class="fab fa-soundcloud"></i>Soundcloud</Button> 
           <Button href="https://open.spotify.com/artist/00RTzPyOnRKHjxlfR8f4AB" target="_blank"><i class="fab fa-spotify"></i>Spotify</Button> 
           <Button href="https://music.apple.com/us/artist/heya-%E3%85%8E%E3%85%87/1390275253" target="_blank"><i class="fab fa-apple"></i>Apple Music</Button>
-          <Button href="https://bandcamp.com/heyabird" target="_blank"><i class="fab fa-bandcamp"></i>Bandcamp</Button>  
+          <Button href="https://heyaworld.bandcamp.com/" target="_blank"><i class="fab fa-bandcamp"></i>Bandcamp</Button>  
       </h4>
         <div id="heya-cartoon">
           <img src="/heya_construction_outside.svg" onMouseOver={e => (e.currentTarget.src = "/heya_construction_inside.svg")} onMouseOut={e => (e.currentTarget.src = "/heya_construction_outside.svg")}/>
